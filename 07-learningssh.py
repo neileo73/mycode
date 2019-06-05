1c1237730ecc40075c7de50c4b81ecc200fa34b6
Neil Debah <ndebah@optonline.net>
2019-06-05 14:21:07 -0400

    parsing info from servers

---
+++ 07-learningssh.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+  
+import os
+
+import paramiko
+
+SRVRS = [{'ip':'10.10.2.3', 'un': 'bender'}, {'ip':'10.10.2.4', 'un': 'fry'}]
+
+with open("cmds2issue.txt", "r") as cmds:
+    CMDLIST = cmds.readlines()
+
+def cmdissue(sshsession, commandtoissue):
+    ssh_stdin, ssh_stdout, ssh_stderr = sshsession.exec_command(commandtoissue)
+    return ssh_stdout.read().decode('utf-8').rstrip('\n')
+
+def main():
+    # harvest RSA key (ssh priv key)
+    myprivkey = paramiko.RSAKey.from_private_key_file("/home/student/.ssh/id_rsa")
+
+    for server in SRVRS:
+        # init connection to remote machine
+        sshsession = paramiko.SSHClient()
+        sshsession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        sshsession.connect(hostname=server['ip'], username=server['un'], pkey=myprivkey)
+
+        # touch two files 
+
+        # get uptime of server
+        for commandtoissue in CMDLIST:
+            result = cmdissue(sshsession, commandtoissue)
+            if result != "":
+                with open((server['ip']).replace(".", "") + ".log", "a") as srvlog:
+                    print("COMMAND ISSUED - ", commandtoissue, file=srvlog)
+
+
+                    print(result, file=srvlog)
+
+        # close the connection
+        sshsession.close()
+
+if __name__ == '__main__':
+    main()
